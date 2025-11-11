3a019a74a19a665458017ceac37ff7bd5b9e4c9c
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-11 12:34:20 +0200

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,7 @@
-const C ="C is fun"
-const Python = "Python is cool"
-const Javascript = "JavaScript is amazing"
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
 
-console.log("C");
-console.log("Python");
-console.log("Javascript");
+console.log(line1);
+console.log(line2);
+console.log(line3);
